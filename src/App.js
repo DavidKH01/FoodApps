2bd65adfcf922300099073f5f5ad7ae072c5065b
Leek Swan <lixuanron@gmail.com>
2022-06-13 22:48:29 -0400

    link front to back

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      test: [],
+      cheapFoods: [],
+    }
+
+  }
+
+  componentDidMount() {
+    axios.get('http://127.0.0.1:5000/')
+    .then(res => {
+      const test = res.data.response;
+        this.setState({ test });
+     })
+
+     axios.get('http://127.0.0.1:5000/getcheapfoods')
+    .then(res => {
+      const cheapFoods = res.data.response;
+        this.setState({ cheapFoods});
+     })
+  }
+
+
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <ul>
+          {
+            this.state.cheapFoods
+              .map(foods =>
+                <li>{foods}</li>
+              )
+          }
+          </ul>
+          <p>
+            {this.state.cheapFoods}
+          </p>
+          <p className='p1'>this is test</p>
+          <p className='p2'>this is test</p>
+        </header>
+      </div>
+    );
+  }
 }
+
+  
+
 
 export default App;
